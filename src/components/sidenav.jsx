91548456de539cdc7aecb7a1b631ad1982ac686d
Kevin Rice <reatriniti@gmail.com>
2023-11-18 20:32:03 -0500

    completed mobile nav in Sidenav.jsx

--- src/components/sidenav.jsx
+++ src/components/sidenav.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { AiOutlineMenu, AiOutlineHome } from "react-icons/ai";
-
+import { AiOutlineMenu, AiOutlineHome, AiOutlineProject, AiOutlineMail } from "react-icons/ai";
+import { BsPerson } from "react-icons/bs";
+import { GrProjects } from "react-icons/gr";
 const SideNav = () => {
   const [nav, setNav] = useState(false);
   const handleNav = () => {
@@ -18,19 +19,19 @@ const SideNav = () => {
              <span >Home</span>
             </a>
             <a href="#main" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200">
-             <AiOutlineHome size={20}/> 
+             <BsPerson size={20}/> 
              <span >About Me</span>
             </a>
             <a href="#main" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200">
-             <AiOutlineHome size={20}/> 
+             <GrProjects size={20}/> 
              <span >Portfolio</span>
             </a>
             <a href="#main" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200">
-             <AiOutlineHome size={20}/> 
+             <AiOutlineMail size={20}/> 
              <span >Contact</span>
             </a>
             <a href="#main" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200">
-             <AiOutlineHome size={20}/> 
+             <AiOutlineProject size={20}/> 
              <span >Resume</span>
             </a>
           </div>
