986537804e68817b03bc7d0c65a8264f9bffa5e2
Kevin Rice <reatriniti@gmail.com>
2023-11-19 15:40:44 -0500

    added TypeAnimation component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { TypeAnimation } from "react-type-animation";
 
 const Header = () => {
     return (
@@ -7,9 +8,29 @@ const Header = () => {
         <div className="w-full h-screen absolute top-0 left-0 bg-white/50" >
         <div>
             <h1>I'm Kevin Rice</h1>
-            <h2>I am </h2>
+            <h2>I am
+            <TypeAnimation
+      sequence={[
+        // Same substring at the start will only be typed out once, initially
+        'We produce food for Mice',
+        1000, // wait 1s before replacing "Mice" with "Hamsters"
+        'We produce food for Hamsters',
+        1000,
+        'We produce food for Guinea Pigs',
+        1000,
+        'We produce food for Chinchillas',
+        1000
+      ]}
+      wrapper="span"
+      speed={50}
+      style={{ fontSize: '2em', display: 'inline-block' }}
+      repeat={Infinity}
+    />
 
-            
+                
+             </h2>
+
+
         </div>
         </div>
     </div>
