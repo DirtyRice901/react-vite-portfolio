95879046583e0ba08a34c069ab81781e9320373e
Kevin Rice <reatriniti@gmail.com>
2023-11-19 16:48:24 -0500

    Header.jsx complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,35 +1,40 @@
 import React from "react";
 import { TypeAnimation } from "react-type-animation";
+import { FaSlack, FaGithub, } from "react-icons/fa";
 
 const Header = () => {
     return (
         <div id="header">
-        <img className="w-full h-screen object-cover" src="Image/gas_mask_copy.jpg" alt="" />
+        <img className="w-full h-screen object-cover" src="Image/gas_mask_copy.jpg" 
+        alt="/" 
+        />
         <div className="w-full h-screen absolute top-0 left-0 bg-white/50" >
-        <div>
-            <h1>I'm Kevin Rice</h1>
-            <h2>I am
+        <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center ">
+            <h1 className="sm:text-5xl text-4xl font-bold text-gray-800">I'm Kevin Rice</h1>
+            <h2 className="flex sm:text-3xl">I am
             <TypeAnimation
       sequence={[
         // Same substring at the start will only be typed out once, initially
-        'We produce food for Mice',
-        1000, // wait 1s before replacing "Mice" with "Hamsters"
-        'We produce food for Hamsters',
-        1000,
-        'We produce food for Guinea Pigs',
-        1000,
-        'We produce food for Chinchillas',
-        1000
+        'Full Stack Developer',
+        2000, // wait 2s before replacing "Mice" with "Hamsters"
+        'Coder',
+        2000,
+        'Tech Enthusiast',
+        2000,
+        'React Developer',
+        2000
       ]}
       wrapper="span"
+      cursor={true}
       speed={50}
-      style={{ fontSize: '2em', display: 'inline-block' }}
+      style={{ fontSize: '1em', paddingLeft: '5px'  }}
       repeat={Infinity}
-    />
-
-                
-             </h2>
-
+            />    
+            </h2>
+            <div className="flex justify-between pt-6 max-w-[80px] w-full">
+                <FaSlack className="cursor-pointer" size={20}/> 
+                <FaGithub className="cursor-pointer" size={20}/> 
+            </div>
 
         </div>
         </div>
