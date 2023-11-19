f72b6ada86bea030fd70b081e911ba8be2aaf95c
Kevin Rice <reatriniti@gmail.com>
2023-11-19 15:24:35 -0500

    began Header.jsx styling and installed react-type-animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,15 @@ import React from "react";
 const Header = () => {
     return (
         <div id="header">
-        <img className="" src="Image/gas_mask_copy.jpg" alt="" />
+        <img className="w-full h-screen object-cover" src="Image/gas_mask_copy.jpg" alt="" />
+        <div className="w-full h-screen absolute top-0 left-0 bg-white/50" >
+        <div>
+            <h1>I'm Kevin Rice</h1>
+            <h2>I am </h2>
+
+            
+        </div>
+        </div>
     </div>
     );
 };
