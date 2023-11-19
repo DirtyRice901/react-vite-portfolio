a6c308670f54e8efb57a8781274abf630c23b13d
Kevin Rice <reatriniti@gmail.com>
2023-11-19 14:19:15 -0500

    side nav completed

--- src/components/sidenav.jsx
+++ src/components/sidenav.jsx
@@ -22,24 +22,43 @@ const SideNav = () => {
              <BsPerson size={20}/> 
              <span >About Me</span>
             </a>
-            <a href="#main" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200">
+            <a href="#portfolio" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200">
              <GrProjects size={20}/> 
              <span >Portfolio</span>
             </a>
-            <a href="#main" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200">
+            <a href="#contact" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200">
              <AiOutlineMail size={20}/> 
              <span >Contact</span>
             </a>
-            <a href="#main" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200">
+            <a href="#resume" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200">
              <AiOutlineProject size={20}/> 
              <span >Resume</span>
             </a>
           </div>
-        )
-        : (
-          <div></div>
+        ) : (
+          ""  
         )
       }
+      <div className="md:block hidden fixed top-[25%] z-10">
+        <div className="flex flex-col">
+          <a href="#main" className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease_in duration-300">
+            <AiOutlineHome size={20} />
+          </a>
+          <a href="#main" className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease_in duration-300">
+            <BsPerson size={20} />
+          </a>
+          <a href="#portfolio" className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease_in duration-300">
+            <GrProjects size={20} />
+          </a>
+          <a href="#contact" className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease_in duration-300">
+            <AiOutlineMail size={20} />
+          </a>
+          <a href="#resume" className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease_in duration-300">
+            <AiOutlineProject size={20} />
+          </a>
+        </div>
+      </div>
+
     </div>
   );
 };
