b209e5134ad92188b7ce1e211f8b11a131f5a68f
Kevin Rice <reatriniti@gmail.com>
2023-11-20 22:22:23 -0500

    word-level typing animation added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,29 +13,39 @@ const Header = () => {
             <h1 className="sm:text-5xl text-4xl font-bold text-gray-200 text-center">I'm Kevin Rice</h1>
             <h2 className="flex sm:text-3xl text-gray-200">
             <TypeAnimation
-                style={{
-                    height: '200px',
-                    width: '600px',
-                    
-                    fontSize: '1em',
-                    paddingLeft: '5px',
-                }}
+                splitter={(str) => str.split(/(?= )/)} // 'Lorem ipsum dolor' -> ['Lorem', ' ipsum', ' dolor']
                 sequence={[
-                // Same substring at the start will only be typed out once, initially
-                    'I am an aspiring Full Stack Developer, inspired to learn and grow',
-                    2000, // wait 2s before replacing "Mice" with "Hamsters"
-                    'Coder',
-                    2000,
-                    'Tech Enthusiast',
-                    2000,
-                    'React Developer',
-                    2000
+                  'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+                  6000,
+                  '',
                 ]}
-                wrapper="span"
-                cursor={true}
-                speed={50}
+                speed={{ type: 'keyStrokeDelayInMs', value: 200 }}
+                omitDeletionAnimation={true}
+                style={{ fontSize: '1em',  width: '900px', textAlign: 'center', color: 'white', }}
                 repeat={Infinity}
-                deletionSpeed={90}
+                // style={{
+                //     height: '200px',
+                //     width: '600px',
+                    
+                //     fontSize: '1em',
+                //     paddingLeft: '2px',
+                // }}
+                // sequence={[
+                // // Same substring at the start will only be typed out once, initially
+                //     'I am an aspiring Full Stack Developer, inspired to learn and grow',
+                //     2000, // wait 2s before replacing "Mice" with "Hamsters"
+                //     'Coder',
+                //     2000,
+                //     'Tech Enthusiast',
+                //     2000,
+                //     'React Developer',
+                //     2000
+                // ]}
+                // wrapper="span"
+                // cursor={true}
+                // speed={50}
+                // repeat={Infinity}
+                // deletionSpeed={90}
             />    
             </h2>
             <div className="flex justify-between items-center pt-6 max-w-[80px] w-full text-gray-200">
